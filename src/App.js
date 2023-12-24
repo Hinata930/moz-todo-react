8fd9ac60b2c3f217ccfa7f6d11990433431566ec
Hinata930 <srgcc4@gmail.com>
2023-12-25 08:58:37 +0900

    delete import App.css and logo.svg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App(props) {
-  {/* const subject = "React"; */}
-  console.log(props);
+  const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>Hello, {subject}!</p> */}
+        <p>Hello, {subject}!</p>
       </header>
     </div>
   );
